53c995c7ba9ca650ac028e3f7e439ae135c4c5f6
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-03-06 02:40:06 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,7 +16,9 @@ export default class Body extends React.Component {
                 <Helmet>
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
                     <meta charSet="utf-8"/>
-                    <meta name="description" content="{_.get(this.props, 'pageContext.frontmatter.excerpt') && _.get(this.props, 'pageContext.frontmatter.excerpt') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.excerpt')}"/>
+                    <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt') && _.get(this.props, 'pageContext.frontmatter.excerpt') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.excerpt')}/>
+                    <meta name="description" content='{_.get(this.props, 'pageContext.frontmatter.excerpt') && _.get(this.props, 'pageContext.frontmatter.excerpt') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.excerpt')}'/>
+
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
 
