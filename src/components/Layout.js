e48c3fd9d2717d89756acf9c93b44e0c2f747ea0
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-03-06 10:40:56 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -27,7 +27,7 @@ export default class Body extends React.Component {
                         
                         
                         
-<meta property="og:url"                content={_.get(this.props, 'pageContext.frontmatter.url')} />
+<meta property="og:url"                content={_.get(this.props, 'pageContext.frontmatter.seourl')} />
 <meta property="og:type"               content="article" />
 <meta property="og:title"              content={_.get(this.props, 'pageContext.frontmatter.title')} />
 <meta property="og:description"        content={_.get(this.props, 'pageContext.frontmatter.excerpt')} />
