5f687f1b934a15d7bb90f3ad0d517987479f3b7a
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-03-07 19:50:54 +0530

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,27 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+_.each(posts, (post, index) => {
+  const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+  const next = index === 0 ? null : posts[index - 1].node;
+
+  createPage({
+    path: post.node.fields.slug,
+    component: path.resolve('./src/templates/post.js'),
+    context: {
+      slug: post.node.fields.slug,
+      previous,
+      next,
+    },
+  })
+
+  createPage({
+    path: `${post.node.fields.slug}/amp`,
+    component: path.resolve('./src/templates/post.amp.js'),
+    context: {
+      slug: post.node.fields.slug,
+      previous,
+      next,
+    },
+  })
+})
