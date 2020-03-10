207e966d5494360587cb1db61c48c99c599cd9cc
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-03-10 16:28:43 +0530

    Update testdocsmenu.js

--- src/components/testdocsmenu.js
+++ src/components/testdocsmenu.js
@@ -15,8 +15,48 @@ export default class DocsMenu extends React.Component {
               <div id="docs-nav-inside" className="docs-nav-inside sticky">
                 <button id="docs-nav-toggle" className="docs-nav-toggle">Navigate Docs<span className="icon-angle-right" aria-hidden="true" /></button>
                 <div className="docs-toc-wrap">
-                  <ul id="docs-toc" class="docs-toc"><li class="toc-item current"><a aria-current="page" class="head" href="/docs/">Javascript</a></li><li class="toc-item has-children"><a href="/docs/about/">About</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button><ul class="toc-submenu"><li class="toc-item"><a href="/docs/about/overview/">Test</a></li><li class="toc-item"><a href="/docs/about/features/">Spiderman</a></li></ul></li><li class="toc-item has-children"><a href="/docs/getting-started/">Superman Started</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button><ul class="toc-submenu"><li class="toc-item"><a href="/docs/getting-started/installation/">Installation</a></li><li class="toc-item"><a href="/docs/getting-started/quick-start/">Quick Start</a></li></ul></li><li class="toc-item has-children"><a href="/docs/ui-components/">UI Components</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button><ul class="toc-submenu"><li class="toc-item"><a href="/docs/ui-components/typography/">Typography</a></li></ul></li><li class="toc-item"><a href="/docs/manage-content/">Manage Content</a></li><li class="toc-item has-children"><a href="/docs/tools/">Tools</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button><ul class="toc-submenu"><li class="toc-item"><a href="/docs/tools/starter-theme/">Starter Theme</a></li><li class="toc-item"><a href="/docs/tools/plug-ins/">Plug-ins</a></li></ul></li><li class="toc-item"><a href="/docs/faq/">FAQ</a></li><li class="toc-item"><a href="/docs/community/">Where To Get Support</a></li></ul>
-                </div>
+                  
+            <ul id="docs-toc" class="docs-toc">
+   <li class="toc-item current"><a aria-current="page" class="head" href="/docs/">Javascript</a></li>
+   <li class="toc-item has-children">
+      <a href="/docs/about/">HTML Basics</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button>
+      <ul class="toc-submenu">
+			 <li class="toc-item"><a href="/docs/about/overview/">HTML Introduction</a></li>
+			 <li class="toc-item"><a href="/docs/about/features/">HTML Getting Started</a></li>
+			 <li class="toc-item"><a href="/docs/about/features/">HTML Elements</a></li>
+			 <li class="toc-item"><a href="/docs/about/features/">HTML Elements</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Attributes</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Headings</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Paragraphs</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Links</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Text Formatting</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Styles</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Images</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Tables</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Lists</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Forms</a></li>
+			<li class="toc-item"><a href="/docs/about/features/">HTML Iframes</a></li>
+
+		
+      </ul>
+   </li>
+   <li class="toc-item has-children">
+      <a href="/docs/getting-started/">HTML Advanced</a><button class="submenu-toggle"><span class="screen-reader-text">Submenu</span><span class="icon-angle-right" aria-hidden="true"></span></button>
+      <ul class="toc-submenu">
+         <li class="toc-item"><a href="/docs/getting-started/installation/">HTML Doctypes</a></li>
+         <li class="toc-item"><a href="/docs/getting-started/quick-start/">HTML Layout</a></li>
+		 <li class="toc-item"><a href="/docs/about/features/">HTML Head</a></li>
+		 <li class="toc-item"><a href="/docs/about/features/">HTML Meta</a></li>
+		 <li class="toc-item"><a href="/docs/about/features/">HTML Scripts</a></li>
+		 <li class="toc-item"><a href="/docs/about/features/">HTML Entites</a></li>
+		 <li class="toc-item"><a href="/docs/about/features/">HTML URL</a></li>
+		 <li class="toc-item"><a href="/docs/about/features/">HTML URL Encode</a></li>
+		 <li class="toc-item"><a href="/docs/about/features/">HTML Validation</a></li>
+      </ul>
+   </li>
+   
+</ul>
+            </div>
               </div>
             </nav>
         );
