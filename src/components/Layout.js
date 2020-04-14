f4fbc1a5c0592cc3dadacf819b19f2e35ca7693d
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-04-14 17:26:47 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,28 +18,63 @@ export default class Body extends React.Component {
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
                     <meta charSet="utf-8"/>
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt')}/>
-                           
+                    
+					<script src="https://code.iconify.design/1/1.0.5/iconify.min.js"></script>       
 
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
+					<link rel="canonical" href={_.get(this.props, 'pageContext.frontmatter.url')} />
+						
+                        
+                        
+                     
+                        
+                        
+						
+	
 
-                        
-                        
-                        
-                        
-<meta property="og:url"                content={_.get(this.props, 'pageContext.frontmatter.seourl')} />
-<meta property="og:type"               content="article" />
-<meta property="og:title"              content={_.get(this.props, 'pageContext.frontmatter.title')} />
-<meta property="og:description"        content={_.get(this.props, 'pageContext.frontmatter.excerpt')} />
-<meta property="og:image"              content={_.get(this.props, 'pageContext.frontmatter.image')} />
-                        
-  <meta name="twitter:card" content="summary" />
-<meta name="twitter:site" content="@flickr" />
-<meta name="twitter:title" content={_.get(this.props, 'pageContext.frontmatter.title')} />
-<meta name="twitter:description" content={_.get(this.props, 'pageContext.frontmatter.excerpt')} />
-<meta name="twitter:image" content={_.get(this.props, 'pageContext.frontmatter.image')} />                      
-                        
-                        
+
+     <meta property="og:locale" content="en_US"/>
+      <meta property="og:type" content="article"/>
+      <meta property="og:title" content={_.get(this.props, 'pageContext.frontmatter.title')}/>
+      <meta property="og:description" content={_.get(this.props, 'pageContext.frontmatter.excerpt')}/>
+      <meta property="og:url" content="https://codevarsity.org/html-article-tag/"/>
+      <meta property="og:site_name" content="Codevarsity"/>
+      <meta property="article:publisher" content="https://www.facebook.com/thecodevarsity/"/>
+      <meta property="article:tag" content="html-article"/>
+      <meta property="article:section" content="HTML"/>
+      <meta property="article:published_time" content={_.get(this.props, 'pageContext.frontmatter.published')}/>
+      <meta property="article:modified_time" content={_.get(this.props, 'pageContext.frontmatter.modified')}/>
+      <meta property="og:updated_time" content={_.get(this.props, 'pageContext.frontmatter.updated')}/>
+      <meta property="fb:app_id" content="1701072829970869"/>
+      <meta property="og:image" content={_.get(this.props, 'pageContext.frontmatter.image')}/>
+      <meta property="og:image:secure_url" content={_.get(this.props, 'pageContext.frontmatter.image')}/>
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta name="twitter:description" content={_.get(this.props, 'pageContext.frontmatter.excerpt')}/>
+      <meta name="twitter:title" content={_.get(this.props, 'pageContext.frontmatter.title')}/>
+      <meta name="twitter:site" content="@thecodevarsity"/>
+      <meta name="twitter:image" content={_.get(this.props, 'pageContext.frontmatter.image')}/>
+      <meta name="twitter:creator" content="@thecodevarsity"/>
+
+
+
+
+
+
+	
+						
+						
+						
+						
+						
+		 
+    
+						
+						
+						
+						
+						
+						
                         
                         
                         
